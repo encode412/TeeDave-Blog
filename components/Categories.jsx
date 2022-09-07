d7fab9b1ae2b7d34ed630be2601387588bbaef36
Timothy Austin <timothyonyea@gmail.com>
2022-09-07 10:47:03 +0100

    error fix

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -8,9 +8,8 @@ const Categories = () => {
   useEffect(() => {
     getCategories()
     .then((newCategories) => setCategories(newCategories))
-  },
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-   [])
+  // eslint-disable-next-line react-hooks/exhaustive-deps  
+}, [])
   return (
     <div className='bg-white shadow-lg rounded-lg p-8 mb-8 pb-12'>
       <h3 className='text-xl mb-8 font-semibold border-b pb-4'>
--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -23,9 +23,8 @@ const PostWidget = ({ categories, slug }) => {
   useEffect(() => {   
     getOlderPosts()
     .then((result) => setOlderPosts(result))
-  },
-  // eslint-disable-next-line react-hooks/exhaustive-deps 
-  [])
+  // eslint-disable-next-line react-hooks/exhaustive-deps  
+ }, [])
   
   
   return (
