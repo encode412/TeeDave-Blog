f43e8fa11bb19739a0a38ffc2e181956436b3d56
Timothy Austin <timothyonyea@gmail.com>
2022-09-09 03:30:23 +0100

    modified iframe size for mobile

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -10,7 +10,7 @@ const MatchDay = () => {
       <h1 className='text-lg lg:text-3xl text-white text-center font-bold'>Match-Day Live!!</h1>
       
       <div className='flex justify-center align-center'>
-        <iframe src="https://live.golkoralive.com/p/ksa1.html" className='border-solid border-4 h-52 lg:h-96'name="myiFrame" scrolling="no" frameBorder="1" marginHeight="0px" marginWidth="0px" width="1000px" allowfullscreen></iframe>
+        <iframe src="https://live.golkoralive.com/p/ksa1.html" className='border-solid border-4 h-[34rem] lg:h-96'name="myiFrame" scrolling="no" frameBorder="1" marginHeight="0px" marginWidth="0px" width="1000px" allowFullscreen></iframe>
       </div>
       <div id="LB24_LIVE_CONTENT" data-eid="3134745359077635842"></div>
       <Script src="https://v.24liveblog.com/24.js" />
