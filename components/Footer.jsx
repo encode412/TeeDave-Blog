4e1ca8db92a692d64fa9287cdf17040459ac2689
Timothy Austin <timothyonyea@gmail.com>
2022-09-19 07:41:05 +0100

    modified footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <>
-     <span className='flex justify-center text-white'><span className='font-semibold'>TeeDave-</span> CopyRight © 2022 Timothy Austin. All Rights Reserved</span>   
+     <div className='flex justify-center p-4 pl-5 text-white'><span className='font-semibold'>TeeDave-</span> CopyRight © 2022 Timothy Austin. All Rights Reserved</div>   
     </>
   )
 }
